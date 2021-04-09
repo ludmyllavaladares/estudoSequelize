1540565ed948d770c5ce7f9e6b4f1336661faa5b
ludmyllavaladares <ludmylla.valadares@a.ficr.edu.br>
2021-04-09 15:29:10 -0300

    Iniciando o Sequelize

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "username": "root",
+    "password": "Pedrolucas01!",
+    "database": "instagram",
+    "host": "127.0.0.1",
+    "dialect": "mysql", 
+    "port": "3306"
+  }
+
+
+
---
+++ instagram.js
@@ -0,0 +1,16 @@
+const { Usuario, Post, Comentario, sequelize } = require('./models');
+
+// Usuario.findAll()
+// .then((resultado) => {
+//     console.log(resultado.map(user => user.toJSON()));    
+// });
+
+// Post.findAll()
+// .then((resultado) => {
+//     console.table(resultado.map(user => user.toJSON()));
+// });
+
+Comentario.findAll()
+.then((resultado) => {
+    console.log(resultado.map(user => user.toJSON()));
+});
